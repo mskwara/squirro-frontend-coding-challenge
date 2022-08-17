4780314eebadd5bb0d4ef6d0456728173d104cf9
Michał Skwara <michal.skwara@stxnext.pl>
2022-08-17 11:57:44 +0200

    create TwoColumnTable component

---
+++ client/src/components/TwoColumnTable/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface IRow {
+  id: number;
+  left: string;
+  right: string;
+}
+
+interface Props {
+  title: string;
+  rows: IRow[];
+}
+
+const TwoColumnTable: React.FC<Props> = ({ title, rows }) => {
+  return (
+    <table className={styles.root}>
+      <thead>
+        <tr>
+          <th colSpan={2}>
+            {title}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(({ id, left, right }) => (
+          <tr key={id}>
+            <td>{left}</td>
+            <td>{right}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TwoColumnTable;
